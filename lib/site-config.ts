00e71c56e9faf492eab0a1c1013845f9f370adf9
ParasKatoch <109639620+ParasKatoch@users.noreply.github.com>
2026-06-01 02:49:55 +0530

    Update site-config.ts

--- lib/site-config.ts
+++ lib/site-config.ts
@@ -46,7 +46,7 @@ export function getSiteConfig(): SiteConfig {
     ),
     copyText: env(
       "NEXT_PUBLIC_COPY_TEXT",
-      "WATCH THIS SPACE FOR CONTRACT ADDRESS",
+      "WATCH THIS SPACE FOR CA",
     ),
     socials: {
       x: env("NEXT_PUBLIC_SOCIAL_X", "https://x.com/bethepump"),
